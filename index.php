22bae4aff281a13f7b61ee1cbe0f138a82e43f48
MichaelN16 <michaelnathan737@gmail.com>
2024-10-01 15:30:22 +0700

    menambahkan lsit

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci sit doloribus dicta quam delectus perferendis, quia ipsam perspiciatis veritatis, corporis vero. Id sunt voluptate delectus aliquam enim mollitia. Repudiandae, quos!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
